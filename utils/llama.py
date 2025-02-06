b90a0ac396387651817cff17ad9a10f003a9702e
muhd.anas70pe@gmail.com <96944434+AlphaAnas@users.noreply.github.com>
2025-02-07 01:46:50 +0500

    added LLM

--- utils/llama.py
+++ utils/llama.py
@@ -1,26 +1,26 @@
-# import os
-# from dotenv import load_dotenv
-# from groq import Groq
+import os
+from dotenv import load_dotenv
+from groq import Groq
 
 
-# load_dotenv()
-# api_key = os.getenv("GROQ_API_KEY")
-# client = Groq(api_key=api_key)
+load_dotenv()
+api_key = os.getenv("GROQ_API_KEY")
+client = Groq(api_key=api_key)
 
-# def get_response(prompt):
+def get_response(prompt):
       
 
-#     response_of_api = client.chat.completions.create(
-#                     model="llama3-70b-8192",
-#                     messages=[
-#                         {"role": "system", "content": "You are a legal assistant."},
-#                         {"role": "user", "content": prompt}
-#                     ],
-#                     max_tokens=1500,
-#                     temperature=0.5
-#                 )
+    response_of_api = client.chat.completions.create(
+                    model="llama3-70b-8192",
+                    messages=[
+                        {"role": "system", "content": "You are a legal assistant."},
+                        {"role": "user", "content": prompt}
+                    ],
+                    max_tokens=1500,
+                    temperature=0.5
+                )
 
-#                 # Extract response content for this chunk
-#     response_text = response_of_api.choices[0].message.content
-#     return response_text
+                # Extract response content for this chunk
+    response_text = response_of_api.choices[0].message.content
+    return response_text
      
